a768c7210f8398a996c4edec47500f92689c0881
Yvonne M Pham <yvonnersz@gmail.com>
2020-08-07 09:28:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 // Code your solution here
 
 let totalBatteries = function (batteryBatches) {
-    return batteryBatches.reduce(total, battery, 0)
+  batteryBatches.reduce((total, battery) => total += battery) 
 }
